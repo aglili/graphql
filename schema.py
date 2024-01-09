616ff2964f8b139fc2e531aff1f5038899c5d6a2
aglili <cecilselorm34@gmail.com>
2024-01-09 09:06:43 +0000

    Add TodoModel and get_db imports to schema.py

--- schema.py
+++ schema.py
@@ -2,4 +2,7 @@ from typing import List
 from graphene import ObjectType,Field,String,Int,ID
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
+from .models import Todo as TodoModel
+from .database_config import get_db
+
 
