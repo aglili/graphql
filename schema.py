0023e5b9ad865bcd69f98daef8309f3e6da3062f
aglili <cecilselorm34@gmail.com>
2024-01-13 09:37:44 +0000

    Add CreateTodo, UpdateTodo, and DeleteTodo mutations

--- schema.py
+++ schema.py
@@ -26,5 +26,62 @@ class Query(ObjectType):
         db = get_db()
         return db.query(TodoModel).all()
     
+
+
+
+class CreateTodo(ObjectType):
+    todo = Field(Todo)
+
+    class Arguments:
+        title = String(required=True)
+        description = String(required=True)
+
+    def mutate(self,info,title,description):
+        db = get_db()
+        todo = TodoModel(title=title,description=description)
+        db.add(todo)
+        db.commit()
+        db.refresh(todo)
+        return CreateTodo(
+            todo=todo
+        )
     
 
+class UpdateTodo(ObjectType):
+    todo = Field(Todo)
+
+
+    class Arguments:
+        id = ID(required=True)
+        title = String()
+        description = String()
+
+    def mutate(self,info,id,title=None,description=None):
+        db = get_db()
+        todo = db.query(TodoModel).filter(TodoModel.id == id).first()
+        if title:
+            todo.title = title
+        if description:
+            todo.description = description
+        db.commit()
+        db.refresh(todo)
+        return UpdateTodo(
+            todo=todo
+        )
+    
+class DeleteTodo(ObjectType):
+    todo = Field(Todo)
+
+    class Arguments:
+        id = ID(required=True)
+
+    def mutate(self,info,id):
+        db = get_db()
+        todo = db.query(TodoModel).filter(TodoModel.id == id).first()
+        if not todo:
+            raise Exception("Todo not found")
+        db.delete(todo)
+        db.commit()
+        return DeleteTodo(
+            success= f"Todo with id {id} deleted successfully"
+        )
