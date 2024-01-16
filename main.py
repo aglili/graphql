0f0a57304ee0d8118ba8e231bab738f9c8a65f79
aglili <cecilselorm34@gmail.com>
2024-01-16 10:04:55 +0000

    Add FastAPI GraphQL and CORS middleware

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from fastapi import FastAPI
+from fastapi_graphql import GraphQLApp
+from starlette.middleware.cors import CORSMiddleware
+
+
+
+from .schema import Query,CreateTodo,DeleteTodo,UpdateTodo
+from .database_config import get_db
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
